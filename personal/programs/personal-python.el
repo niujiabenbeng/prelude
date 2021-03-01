79f6e68160bc541c0dd12ae9308515eeb8156db0
chenli <chen220li@163.com>
2021-03-01 16:36:45 +0800

    fix a bug

--- personal/programs/personal-python.el
+++ personal/programs/personal-python.el
@@ -1,28 +1,67 @@
 ;;; personal-python.el --- Personal configuration for Python.
 
 ;;; Commentary:
-;;; Personal configuration for Python.
+
+;; Personal configuration for Python.
 
 ;;; Code:
 
 (require 'python)
-(require 'prelude-python)
+(require 'personal-lsp)
 
-(defun personal-set-python-interpreter ()
-  "Set python interpreter based on `PYTHONENV'."
-  (let ((env (getenv "PYTHONENV")))
-    (when (and env (file-executable-p env))
-      (setq pythonic-interpreter env)
-      (setq python-shell-interpreter env)
-      (pythonic-activate pythonic-interpreter))))
-(add-hook 'python-mode-hook 'personal-set-python-interpreter)
+(defun personal-python-lsp-get-pylint-init-hook (project-root init-file)
+  "Return a piece of python snippet as pylint init-hook."
+  (let ((init-hook ""))
+    ;; 如果project-root存在, 则将project-root放到sys.path中
+    (when (and project-root (file-exists-p project-root))
+      (setq init-hook (concat init-hook (format "\
+import sys
+sys.path.append(\"%s\")
+" (expand-file-name project-root)))))
+    ;; 如果init-file存在, 则加载init-file的内容
+    (when (and init-file (file-exists-p init-file))
+      (setq init-hook (concat init-hook (format "\
+import importlib.util
+spec = importlib.util.spec_from_file_location(\"init\", \"%s\")
+foo = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(foo)
+" (expand-file-name init-file)))))
+    init-hook))
 
-(cond ((eq personal-python-conf 'native)
-       (require 'personal-python-native))
-      ((eq personal-python-conf 'lsp)
-       (require 'personal-lsp)
-       (require 'personal-python-lsp))
-      (t (error "Wrong value of `personal-python-conf'")))
+(defun personal-python-lsp-enable-pylint ()
+  "Enable pylint in lsp."
+  (setq lsp-pyls-plugins-pylint-enabled t)
+  ; make pylint respect subdirs and dynamic import
+  (when-let* ((curr (buffer-file-name))
+              (root (locate-dominating-file curr ".pylintrc"))
+              (conf (expand-file-name ".pylintrc" root))
+              (args (list (format "--rcfile=%s" conf))))
+    (setq curr (file-name-directory curr))
+    (push (format
+           "--init-hook='%s'"
+           (personal-python-lsp-get-pylint-init-hook
+            root (expand-file-name "init.py" curr))) args)
+    (setq lsp-pyls-plugins-pylint-args (vconcat args))))
+
+(defun personal-python-lsp-setup ()
+  "Set python language server based on `PYLSEXEC'."
+  (let ((env (getenv "PYLSEXEC")))
+    (if (and env (file-executable-p env))
+        (setq lsp-pyls-server-command env)
+      (error "python-language-server is not installed.")))
+  (setq lsp-pyls-plugins-mccabe-enabled nil)
+  (setq lsp-pyls-plugins-pylint-enabled nil)
+  (setq lsp-pyls-plugins-pycodestyle-enabled nil)
+  (setq lsp-pyls-plugins-pydocstyle-enabled nil)
+  (setq lsp-pyls-plugins-pyflakes-enabled nil)
+  (setq lsp-pyls-plugins-autopep8-enabled nil)
+  (setq lsp-pyls-plugins-yapf-enabled t)
+  (setq lsp-pyls-plugins-flake8-enabled nil)
+  (personal-python-lsp-enable-pylint)
+  (lsp-deferred))
+
+(add-hook 'python-mode-hook #'personal-python-lsp-setup)
+(add-hook 'python-mode-hook #'personal-yasnippet-all)
 
 (provide 'personal-python)
 
--- personal/programs/personal-shell.el
+++ personal/programs/personal-shell.el
@@ -1,23 +1,24 @@
 ;;; personal-shell.el --- Personal configuration for shell script.
 
 ;;; Commentary:
-;;; Personal configuration for shell script.
+
+;; Personal configuration for shell script.
 
 ;;; Code:
 
 (require 'comint)
 (require 'prelude-shell)
 
-;;; prevent shell echoing
+;; prevent shell echoing
 (defun my-comint-init ()
   (setq comint-process-echoes t))
 (add-hook 'comint-mode-hook 'my-comint-init)
 (add-hook 'shell-mode-hook #'turn-off-auto-fill)
 
-;;; always indent 4 spaces after continuation
+;; always indent 4 spaces after continuation
 (setq sh-indent-after-continuation 'always)
 
-;;; sh-mode imenu支持变量
+;; sh-mode imenu支持变量
 (defun my-shell-mode-setup-imenu ()
   (setq imenu-generic-expression
         (append '(("Variable" "^\\([[:alnum:]_]+\\)=.*" 1))
